a80d293eec8a20ed655a5800c3124c8e2183f6c5
jeffry suero <jeffrysueroc@gmail.com>
2023-09-13 10:19:52 -0400

    update

--- src/controllers/shipping/orderStatus.controller.ts
+++ src/controllers/shipping/orderStatus.controller.ts
@@ -7,7 +7,10 @@ export const orderStatus = async (request: IRequest, response: Response) => {
     const {  DB_PREFIX } = request.body;
     const pool = request.pool
       const status = await statusService(DB_PREFIX,pool);
-      console.log("🚀 ~ file: orderStatus.controller.ts:9 ~ orderStatus ~ status:", status)
+      if(!status) return response.json({ error: { message: 'error update status' } })
+      response.status(200).json({
+        createOrdern: true
+    })
       
 
 }
--- src/models/socket.ts
+++ src/models/socket.ts
@@ -6,40 +6,9 @@ export const Socket = async (io: any) => {
 
     io.on("connection", (socket: any) => {
 
-
-        const status = async () => {
-            setTimeout(async () => {
-                //  const status = await statusService()
-                const cambioEnBaseDeDatos = { propiedad: status };
-                socket.emit('server:status', cambioEnBaseDeDatos);
-
-            }, 5000);
-        }
-
-        status()
         
         console.log("🚀 ~ file: socket.ts:6 ~ io.on ~ socket:", socket.id)
-        const test = async () => {
-            // const [tokenMBE]: any = await pool.query(`select * from ps_mbe_shipping_token`);
-            // socket.emit("server:order", tokenMBE)
-
-        }
-
-        test()
-
-        const update = async () => {
-            // console.log("🚀 ~ file: socket.ts:11 ~ socket.emit ~ tokenMBE:", tokenMBE)
-            socket.on("client:order", async (data: any) => {
-                // const [result]: any = await pool.query(`UPDATE ps_mbe_shipping_token SET token_shipping = ? WHERE id_mbeshipping = ?`, ["1uBuDSruZ66PI_eTAAAM", 1]);
-
-                // const [tokenMBE]: any = await pool.query(`select * from ps_mbe_shipping_token`);
-                // io.emit("server:order", tokenMBE);
-                // console.log("🚀 ~ file: socket.ts:23 ~ socket.on ~ result:", result)
-
-            })
-
-        }
-        update()
+      
 
     })
 
