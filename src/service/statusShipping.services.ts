56ea9124cdf833e04c9ab50ecfd52c24c04225c4
jeffry suero <jeffrysueroc@gmail.com>
2023-09-19 09:51:22 -0400

    update

--- src/service/statusShipping.services.ts
+++ src/service/statusShipping.services.ts
@@ -29,6 +29,7 @@ export const statusService = async (DB_PREFIX:any,pool:any) => {
                    
                     const [getorders]: any = await pool.query(`select * from ${DB_PREFIX}orders where reference = ?`,[data.number]);
                     if (!getorders) return { error: { message: "getorders not fount" } }
+                    console.log("🚀 ~ file: statusShipping.services.ts:32 ~ result.data.map ~ getorders:", getorders)
                     
                     
                     const [ordersCarrier]: any = await pool.query(`UPDATE ${DB_PREFIX}order_carrier SET tracking_number = ?  WHERE id_order = ?`, [data.tracking,getorders[0].id_order]);
