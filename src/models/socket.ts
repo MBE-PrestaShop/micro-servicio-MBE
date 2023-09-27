3aa64f7e6972b15ed9d0a78275d76228dc0dd4f7
jeffry suero <jeffrysueroc@gmail.com>
2023-09-27 16:22:26 -0400

    .

--- src/models/socket.ts
+++ src/models/socket.ts
@@ -5,25 +5,25 @@ export const Socket = async (io: any) => {
 
     io.on("connection", (socket: any) => {
 
-        // const status = async () => {
-        //         socket.on('server:status', async (data:any) => {
-        //             const mysqlFactory = new MySQLConnectionFactory();
+        const status = async () => {
+                socket.on('server:status', async (data:any) => {
+                    const mysqlFactory = new MySQLConnectionFactory();
 
-        //             const pool = await mysqlFactory.createConnection({
-        //                 host: data.DB_SERVER,
-        //                 database: data.DB_NAME,
-        //                 user: data.DB_USER,
-        //                 password: data.DB_PASSWD,
-        //                 port: 3306
-        //             })
+                    const pool = await mysqlFactory.createConnection({
+                        host: data.DB_SERVER,
+                        database: data.DB_NAME,
+                        user: data.DB_USER,
+                        password: data.DB_PASSWD,
+                        port: 3306
+                    })
 
-        //             await statusService(data.DB_PREFIX,pool)
-        //         })
+                    await statusService(data.DB_PREFIX,pool)
+                })
 
           
-        // }
+        }
 
-        // status()
+        status()
         
         socket.on('disconnect', () => {
             console.log('Cliente desconectado');
