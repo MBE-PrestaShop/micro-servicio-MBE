5e268b4957a26c64b2e5d92912b6374bc371732f
jeffry suero <jeffrysueroc@gmail.com>
2023-09-27 12:06:53 -0400

    update

--- src/models/socket.ts
+++ src/models/socket.ts
@@ -1,14 +1,34 @@
 
-// import { pool } from "../db/mysql"
 import { statusService } from "../service/statusShipping.services"
-
+import MySQLConnectionFactory from "../db/mysql";
 export const Socket = async (io: any) => {
 
     io.on("connection", (socket: any) => {
 
+        const status = async () => {
+                socket.on('server:status', async (data:any) => {
+                    const mysqlFactory = new MySQLConnectionFactory();
+
+                    const pool = await mysqlFactory.createConnection({
+                        host: data.DB_SERVER,
+                        database: data.DB_NAME,
+                        user: data.DB_USER,
+                        password: data.DB_PASSWD,
+                        port: 3306
+                    })
+
+                    await statusService(data.DB_PREFIX,pool)
+                })
+
+          
+        }
+
+        status()
         
-        console.log("🚀 ~ file: socket.ts:6 ~ io.on ~ socket:", socket.id)
-      
+        socket.on('disconnect', () => {
+            console.log('Cliente desconectado');
+        });
+    
 
     })
 
