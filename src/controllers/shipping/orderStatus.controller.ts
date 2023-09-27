12f2f7829238bba0bcc1de44553fa16f79f709ae
jeffry suero <jeffrysueroc@gmail.com>
2023-09-27 15:59:53 -0400

    .

--- src/controllers/shipping/orderStatus.controller.ts
+++ src/controllers/shipping/orderStatus.controller.ts
@@ -5,6 +5,7 @@ import { statusService } from "../../service/statusShipping.services"
 export const orderStatus = async (request: IRequest, response: Response) => {
 
     const {  DB_PREFIX } = request.body;
+    console.log("🚀 ~ file: orderStatus.controller.ts:8 ~ orderStatus ~ DB_PREFIX:", DB_PREFIX)
     const pool = request.pool
       const status = await statusService(DB_PREFIX,pool);
       if(!status) return response.json({ error: { message: 'error update status' } })
